c70700c3d8c6ee8f398f30208a79825b5ef919fc
Stefano Weidmann <stefanow@student.ethz.ch>
2019-02-23 23:21:21 +0100

    replace tabs with spaces

--- _posts/2019-02-23-win-hwbreakpoints.markdown
+++ _posts/2019-02-23-win-hwbreakpoints.markdown
@@ -28,17 +28,17 @@ HWBreakpoint::ClearAll();
 
 // or for C
 typedef enum {
-	HWBRK_WRITE = 1,
-	HWBRK_READ_WRITE = 3
+    HWBRK_WRITE = 1,
+    HWBRK_READ_WRITE = 3
 } HWBRK_Condition;
 
 int HWBRK_Set(
-	void* address,
-	HWBRK_Condition when
+    void* address,
+    HWBRK_Condition when
 );
 
 void HWBRK_Clear(
-	void* address
+    void* address
 );
 
 void HWBRK_ClearAll(
--- downloads/hwbreakpoint.h
+++ downloads/hwbreakpoint.h
@@ -3,15 +3,15 @@
 #ifdef __cplusplus
 namespace HWBreakpoint
 {
-	enum class Condition
-	{
-		Write = 1,
-		ReadWrite = 3
-	};
+    enum class Condition
+    {
+        Write = 1,
+        ReadWrite = 3
+    };
 
-	bool Set(void* address, Condition when);
-	void Clear(void* address);
-	void ClearAll();
+    bool Set(void* address, Condition when);
+    void Clear(void* address);
+    void ClearAll();
 };
 #endif
 
@@ -20,17 +20,17 @@ extern "C" {
 #endif
 
 typedef enum {
-	HWBRK_WRITE = 1,
-	HWBRK_READ_WRITE = 3
+    HWBRK_WRITE = 1,
+    HWBRK_READ_WRITE = 3
 } HWBRK_Condition;
 
 int HWBRK_Set(
-	void* address,
-	HWBRK_Condition when
+    void* address,
+    HWBRK_Condition when
 );
 
 void HWBRK_Clear(
-	void* address
+    void* address
 );
 
 void HWBRK_ClearAll(
